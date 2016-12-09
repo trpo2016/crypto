5d0e5514bee62f8b94ca0b0a6cd53c5ccf25f4de
Alexnader Novikov <justjwill90@gmail.com>
2016-12-10 01:16:58 +0700

    add options and index js file

---
+++ index.js
@@ -0,0 +1,82 @@
+const crypto = require('./crypto.js');
+const commandLineArgs = require('command-line-args');
+const getUsage = require('command-line-usage')
+const optionsDef = require('./options.js');
+
+const { ciphers, helpers } = crypto;
+const sections = [
+  {
+    header: 'Crypto CLI',
+    content: 'Encrypts input text/file using some basic ciphers. Application only supports 3 ciphers – Ceasar, Vernam and Vigenere'
+  },
+  {
+    header: 'Synopsis',
+    content: [
+      '$ example [[bold]{--input} [underline]{text}] [bold]{--encrypt} [underline]{ceasar} ...',
+      '$ example [[bold]{--input} [underline]{text}] [bold]{--decrypt} [bold]{--key} path',
+      '$ example [bold]{--help}'
+    ]
+  },
+  {
+    header: 'Options',
+    optionList: optionsDef
+  },
+  {
+    content: "Project home: [underline]{https://github.com/app}"
+  }
+];
+
+const usage = getUsage(sections)
+const options = commandLineArgs(optionsDef);
+
+let c = options.cipher;
+let text = options.input;
+let key = options.key;
+
+const handleCipher = (type, key, text) => {
+  let result;
+
+  if (options.encrypt){
+    result = ciphers[type].encode(key, text);
+  } else {
+    result = ciphers[type].decode(key, text)
+  }
+
+  return result
+}
+
+let result;
+
+switch (c){
+  case 'ceasar': {
+    key = parseInt(key);
+    result = handleCipher('ceasar', key, text);
+    console.log(result);
+
+    break;
+  };
+  case 'vernam': {
+    if (!key) key = ciphers.vernam.generateRandomKey(text);
+    result = handleCipher('vernam', key, text);
+    result = helpers.prettyPrint(result);
+    console.log(result);
+
+    break;
+  };
+  case 'vigenere': {
+    result = handleCipher('vigenere', key, text);
+    console.log(result);
+
+    break;
+  };
+  default: {
+    console.log(usage);
+  }
+};
+
+// const key = ciphers.vernam.generateRandomKey(text);
+// console.log(`Key = ${key}`);
+// const result = ciphers.vernam.encode(key, text);
+// console.log(`Encoded = ${helpers.prettyPrint(result)}`);
+// const decrypted = ciphers.vernam.decode(key, result);
+// console.log(`Decrypted = ${helpers.prettyPrint(decrypted)}`);
---
+++ options.js
@@ -0,0 +1,7 @@
+module.exports = [
+  { name: 'cipher', type: String },
+  { name: 'encrypt', type: Boolean},
+  { name: 'decrypt', type: Boolean },
+  { name: 'input', alias: 'i', type: String },
+  { name: 'key', alias: 'k', type: String }
+];
