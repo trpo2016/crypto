3f8240637ef34be5fcff102777e490d7741a14d7
Alexnader Novikov <justjwill90@gmail.com>
2016-12-11 13:11:46 +0700

    fix errors in tests

--- test.js
+++ test.js
@@ -1,45 +1,50 @@
 const assert = require("assert");
-const crypto = require("crypto");
+const crypto = require("./crypto.js");
 
 const { ciphers, helpers } = crypto;
+const text = "hello world!";
 
 describe("Ceasar", () => {
   describe("#encode()", () => {
     it("should return encoded text for given key, encoded text should always stay the same.", () => {
-      const encoded = ciphers.ceasar.encode(10, "hello world!");
-      assert(encoded, "rovvy gybvn!");
+      const encoded = ciphers.ceasar.encode(10, text);
+      assert.equal(encoded, "rovvy gybvn!");
     });
   });
   describe("#decode()", () => {
-    const decoded = ciphers.ceasar.decode(10, "rovvy gybvn!");
-    assert(decoded, "hello world!");
+    it("should return encoded text for given key, encoded text should always stay the same.", () => {
+      const decoded = ciphers.ceasar.decode(10, "rovvy gybvn!");
+      assert.equal(decoded, text);
+    });
   });
 });
-
 
 describe("Vigenere", () => {
   describe("#encode()", () => {
     it("should return encoded text for given key, encoded text should always stay the same.", () => {
-      const encoded = ciphers.vigenere.encode("abc", "hello world!");
-      assert(encoded, "hfnlp yosnd!");
+      const encoded = ciphers.vigenere.encode("abc", text);
+      assert.equal(encoded, "hfnlp yosnd!");
     });
   });
   describe("#decode()", () => {
-    const decoded = ciphers.vigenere.decode("abc", "hfnlp yosnd!");
-    assert(decoded, "hello world!");
+    it("should return encoded text for given key, encoded text should always stay the same.", () => {
+      const decoded = ciphers.vigenere.decode("abc", "hfnlp yosnd!");
+      assert.equal(decoded, text);
+    });
   });
 });
-
 
 describe("Vernam", () => {
   describe("#encode()", () => {
     it("should return encoded text for given key, encoded text should always stay the same.", () => {
-      const encoded = ciphers.vigenere.encode("abc123", "8;>9#<*=j92c");
-      assert(encoded, "rovvy gybvn!");
+      const encoded = ciphers.vernam.encode("abcdefghijkl", text);
+      assert.equal(encoded, "omeln\"tkwjc)");
     });
   });
   describe("#decode()", () => {
-    const decoded = ciphers.vigenere.decode("abc123", "8;>9#<*=j92c");
-    assert(decoded, "hello world!");
+    it("should return encoded text for given key, encoded text should always stay the same.", () => {
+      const decoded = ciphers.vernam.decode("abcdefghijkl", "omeln\"tkwjc)");
+      assert.equal(decoded, text);
+    });
   });
 });
