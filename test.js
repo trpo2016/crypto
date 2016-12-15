c464ba90ae359a6493c0fb2b7b2aa619c8ac404d
Alexnader Novikov <justjwill90@gmail.com>
2016-12-15 15:29:40 +0700

    fix test text, add scripts for testing and edit README.md

--- test.js
+++ test.js
@@ -9,12 +9,14 @@ describe("Ceasar", () => {
     it("should return encoded text for given key, encoded text should always stay the same.", () => {
       const encoded = ciphers.ceasar.encode(10, text);
       assert.equal(encoded, "rovvy gybvn!");
+      assert.equal(typeof encoded, 'string');
     });
   });
   describe("#decode()", () => {
-    it("should return encoded text for given key, encoded text should always stay the same.", () => {
+    it("should return decoded text for given key, decoded text should always stay the same.", () => {
       const decoded = ciphers.ceasar.decode(10, "rovvy gybvn!");
       assert.equal(decoded, text);
+      assert.equal(typeof decoded, 'string');
     });
   });
 });
@@ -24,12 +26,14 @@ describe("Vigenere", () => {
     it("should return encoded text for given key, encoded text should always stay the same.", () => {
       const encoded = ciphers.vigenere.encode("abc", text);
       assert.equal(encoded, "hfnlp yosnd!");
+      assert.equal(typeof encoded, 'string');
     });
   });
   describe("#decode()", () => {
-    it("should return encoded text for given key, encoded text should always stay the same.", () => {
+    it("should return decoded text for given key, decoded text should always stay the same.", () => {
       const decoded = ciphers.vigenere.decode("abc", "hfnlp yosnd!");
       assert.equal(decoded, text);
+      assert.equal(typeof decoded, 'string');
     });
   });
 });
@@ -39,12 +43,14 @@ describe("Vernam", () => {
     it("should return encoded text for given key, encoded text should always stay the same.", () => {
       const encoded = ciphers.vernam.encode("abcdefghijkl", text);
       assert.equal(encoded, "omeln\"tkwjc)");
+      assert.equal(typeof encoded, 'string');
     });
   });
   describe("#decode()", () => {
-    it("should return encoded text for given key, encoded text should always stay the same.", () => {
+    it("should return decoded text for given key, decoded text should always stay the same.", () => {
       const decoded = ciphers.vernam.decode("abcdefghijkl", "omeln\"tkwjc)");
       assert.equal(decoded, text);
+      assert.equal(typeof decoded, 'string');
     });
   });
 });
